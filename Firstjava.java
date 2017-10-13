0374d81f0634728526e2b232af94e4f2113b8e7a
chen2chenjx <1968488809@qq.com>
2017-10-13 13:55:44 +0800

    write a java code

---
+++ Firstjava.java
@@ -0,0 +1,8 @@
+public class Firstjava
+{
+   public static void main(String[] args)
+           { 
+              System.out.println("First Java program");
+           }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+this is a git tutoria for me
+
